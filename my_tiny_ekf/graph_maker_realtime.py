48e1fab398ce24bc3e2ce43c6f9e432cdd938ec2
DukiChoi <cbcc12345@hanyang.ac.kr>
2023-08-21 03:51:59 +0900

    Now it shows the orientation properly

--- my_tiny_ekf/graph_maker_realtime.py
+++ my_tiny_ekf/graph_maker_realtime.py
@@ -16,6 +16,7 @@ import signal
 import threading
 import ctypes
 from ctypes import *
+import math
 i = c_double(0)
 pi = pointer(i)
 x = ""
@@ -24,6 +25,9 @@ z = ""
 x_val = []
 y_val = []
 z_val = []
+pitch_val = []
+roll_val = []
+yaw_val = []
 text = ""
 
 port = 'COM3'
@@ -31,7 +35,7 @@ port = 'COM3'
 baud = 115200
 ser = Serial(port,baud) 
 exitThread = False
-
+DEG_2_RAD = 0.01745329251;
 
 def animate(i):
     if ser.readable():
@@ -40,48 +44,75 @@ def animate(i):
         
         if "Position" in res.decode()[:len(res)-1]:
             text = res.decode()[:len(res)-1]
-        #print(text)
             text_splited = text.split(",")
             if len(text_splited) > 1:
                 x = text_splited[1]
                 y = text_splited[2]
                 z = text_splited[3]
-                print("X:" + x + ", Y:" + y + ", Z:" + z)
+                pi = text_splited[5]  
+                theta = text_splited[6] 
+                psi = text_splited[7] 
+                
+                print("Position : (" + x + ", " + y + ", " + z + ")")
+                print("Orientation = (" + pi + ", " + theta + ", " + psi + ")")
                 x_val.append(float(x))
                 y_val.append(float(y))
                 z_val.append(float(z))
+                pi = float(pi)*DEG_2_RAD #yaw
+                theta = float(theta)*DEG_2_RAD #pitch
+                psi = float(psi)*DEG_2_RAD #roll
+                
                 plt.cla()
                 drawplots()
                 plt.plot(x_val[len(x_val)-11: len(x_val)-1], y_val[len(y_val)-11: len(y_val)-1], z_val[len(z_val)-11: len(z_val)-1], color = 'g', alpha = 0.6)
                 
                 
                 
-                x1 = [x_val[len(x_val)-1], x_val[len(x_val)-1] + 0.1, x_val[len(x_val)-1] + 0.2]
-                x2 = [y_val[len(y_val)-1], y_val[len(y_val)-1], y_val[len(y_val)-1]]
-                x3 = [z_val[len(z_val)-1], z_val[len(z_val)-1], z_val[len(z_val)-1]]
-                y1 = [x_val[len(x_val)-1], x_val[len(x_val)-1], x_val[len(x_val)-1]]
-                y2 = [y_val[len(y_val)-1], y_val[len(y_val)-1] + 0.1, y_val[len(y_val)-1] + 0.2]
-                y3 = [z_val[len(z_val)-1], z_val[len(z_val)-1], z_val[len(z_val)-1]]
-                z1 = [x_val[len(x_val)-1], x_val[len(x_val)-1], x_val[len(x_val)-1]]
-                z2 = [y_val[len(y_val)-1], y_val[len(y_val)-1], y_val[len(y_val)-1]]
-                z3 = [z_val[len(z_val)-1], z_val[len(z_val)-1] + 0.1, z_val[len(z_val)-1] + 0.2]
+                xx = [[x_val[len(x_val)-1], x_val[len(x_val)-1] + 0.1, x_val[len(x_val)-1] + 0.2],
+                      [y_val[len(y_val)-1], y_val[len(y_val)-1], y_val[len(y_val)-1]],
+                      [z_val[len(z_val)-1], z_val[len(z_val)-1], z_val[len(z_val)-1]]]
+                yy = [[x_val[len(x_val)-1], x_val[len(x_val)-1], x_val[len(x_val)-1]],
+                     [y_val[len(y_val)-1], y_val[len(y_val)-1] + 0.1, y_val[len(y_val)-1] + 0.2],
+                    [z_val[len(z_val)-1], z_val[len(z_val)-1], z_val[len(z_val)-1]]]
+                zz = [[x_val[len(x_val)-1], x_val[len(x_val)-1], x_val[len(x_val)-1]],
+                     [y_val[len(y_val)-1], y_val[len(y_val)-1], y_val[len(y_val)-1]],
+                     [z_val[len(z_val)-1], z_val[len(z_val)-1] + 0.1, z_val[len(z_val)-1] + 0.2]]
                 
+                # rotation_matrix = [[math.cos(theta)*math.cos(psi), -math.cos(theta)*math.sin(psi), math.sin(theta)],
+                #       [math.cos(pi)*math.sin(psi)+math.sin(pi)*math.sin(theta)*math.cos(psi), math.cos(pi)*math.cos(psi) - math.sin(pi)*math.sin(theta)*math.sin(psi), - math.sin(pi)*math.cos(theta)],
+                #       [math.sin(pi)*math.sin(psi)-math.cos(pi)*math.sin(theta)*math.cos(psi), math.sin(pi)*math.cos(psi) + math.cos(pi)*math.sin(theta)*math.sin(psi), math.cos(pi)*math.cos(theta)]]
                 
-                plt.plot(x1,x2,x3, color = 'g')
-                plt.plot(y1,y2,y3, color = 'g')
-                plt.plot(z1,z2,z3 , color = 'g')
+                rotation_matrix = [[-math.cos(theta) * math.cos(pi), -math.sin(psi) * math.sin(theta) * math.cos(-pi) + math.cos(psi) * math.sin(-pi), math.cos(psi) * math.sin(theta) * math.cos(-pi) + math.sin(psi) * math.sin(-pi)],
+                                    [-math.cos(theta) * math.sin(-pi), - math.sin(psi) * math.sin(theta) * math.sin(-pi) - math.cos(psi) * math.cos(-pi), math.cos(psi) * math.sin(theta) * math.sin(-pi) - math.sin(psi) * math.cos(-pi)],
+                                    [math.sin(theta), -math.sin(psi) * math.cos(theta), math.cos(psi) * math.cos(theta)]]
                 
+                # rotation_matrix = [[-math.sin(psi)*math.sin(pi) + math.cos(theta)*math.cos(pi)*math.cos(psi), math.sin(psi)*math.cos(pi) + math.cos(theta)*math.sin(pi)*math.cos(psi), -math.cos(psi)*math.sin(theta)],
+                #                    [-math.cos(psi)*math.sin(pi) - math.cos(theta)*math.cos(pi)*math.sin(psi), math.cos(psi)*math.cos(theta) - math.cos(theta)*math.sin(pi)*math.sin(psi), math.sin(psi)*math.sin(theta)],
+                #                    [math.sin(theta)*math.cos(pi), math.sin(theta)*math.sin(pi), math.cos(theta)]]
+                
+                rotation_matrix = np.array(rotation_matrix)
+                xx = rotation_matrix.dot(np.array(xx))
+                yy = rotation_matrix.dot(np.array(yy))
+                zz = rotation_matrix.dot(np.array(zz))
+
+                plt.plot(xx[0],xx[1],xx[2], color = 'red')
+                plt.plot(yy[0],yy[1],yy[2], color = 'orange')
+                plt.plot(zz[0],zz[1],zz[2] , color = 'yellow')
+        
         else:
             text = res.decode()[:len(res)-1]
             print(text)
-
+            
 def drawplots():
     n = 3
     x = [-0.5, 0, 0.5]
     zero = np.zeros(n)
-    ax.plot(x, zero, zero, color = 'r', alpha = 0.9)
-    ax.plot(zero, x, zero, color = 'r', alpha = 0.9)
-    ax.plot(zero, zero, x, color = 'r', alpha = 0.9)
+    ax.plot(x, zero, zero, color = 'black', alpha = 0.7)
+    ax.plot(zero, x, zero, color = 'black', alpha = 0.7)
+    ax.plot(zero, zero, x, color = 'black', alpha = 0.7)
+    ax.set_xlabel('X_axis')
+    ax.set_ylabel('Y_axis')
+    ax.set_zlabel('Z_axis')
     
         
 if __name__ == "__main__":
